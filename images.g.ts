a21b01255f7bb3ac40dc106b24672cead96625cc
aalborghetti <alberto.alborghetti@gmail.com>
2024-09-15 11:39:18 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "w5Q:[QG,yl6_SidH4X],":
+            case "aereo":return img`
+..ccc.........ffffff....
+..f4cc.......fcc22ff....
+..f44cc...fffccccff.....
+..f244cccc22224442cc....
+..f224cc2222222244b9c...
+..cf2222222222222b999c..
+.c22c222222222b11199b2c.
+f22ccccccc222299111b222c
+fffffcc222c222222222222f
+.....f2222442222222222f.
+....f222244fc2222222ff..
+...c222244ffffffffff....
+...c2222cfffc2f.........
+...ffffffff2ccf.........
+.......ffff2cf..........
+........fffff...........
+`;
+            case "image1":
+            case "proiettile":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 3 3 3 2 . . . . 
+. 2 2 2 3 3 1 1 1 1 1 3 2 . . . 
+. 1 1 1 1 1 1 1 1 1 1 1 2 . . . 
+. 2 2 2 3 3 1 1 1 1 1 3 2 . . . 
+. . . . . 2 2 3 3 3 3 2 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage":return img`
+....ffffff.........ccc..
+....ff88ccf.......cc6f..
+.....ffccccfff...cc66f..
+....cc86668888cccc668f..
+...c9b6688888888cc688f..
+..c999b8888888888888fc..
+.c8b99111b888888888c88c.
+c888b111998888ccccccc88f
+f888888888888c888ccfffff
+.f8888888888668888f.....
+..ff8888888cf668888f....
+....ffffffffff668888c...
+.........f8cfffc8888c...
+.........fcc8ffffffff...
+..........fc8ffff.......
+...........fffff........
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
